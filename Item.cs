eab2ebfbd59cb873fa9035024d9da6cb1c36df71
softwareNuggets <softwareNugget65@gmail.com>
2022-10-02 22:07:23 -0400

    YT video for Validate with Data Annotations

---
+++ Item.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ValidateWithDataAnnotations
+{
+    public class Item
+    {
+
+        [Required]
+        public int ItemId { get; set; }
+
+        [Required]
+        public string ShortDesc { get; set; }
+
+
+
+        
+
+        [Required(ErrorMessage ="Please enter a value for the General Ledger Code [GLCode]")]
+
+        [MinLength(3)]
+        [MaxLength(5)]
+        public string GLCode { get; set; }
+
+
+
+        [Required]
+        public DateTime CreateDate { get; set; }
+
+
+        public List<ValidationResult>? IsItemValid(Item item)
+        {
+            var errorMessage = new List<ValidationResult>();
+
+            var context = new ValidationContext(item);
+
+            bool rv = Validator.TryValidateObject(item, context, errorMessage,true);
+            if(rv == false)
+            {
+                return errorMessage;
+            }
+
+            return null;
+        }
+
+        public void ShowErrors(List<ValidationResult> errors)
+        {
+            foreach(var error in errors)
+            {
+                Console.WriteLine($"error: {error}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ValidateWithDataAnnotations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var item = new Item();
+            item.ItemId = 1;
+            item.ShortDesc = "short item description";
+            item.GLCode ="GLA_0000";
+            item.CreateDate = DateTime.Now;
+
+            
+
+            List<ValidationResult>? errs = item.IsItemValid(item);
+            if(errs != null)
+            {
+                item.ShowErrors(errs);
+                return;
+            }
+
+            InsertIntoTable(item);
+
+        }
+
+        private static void InsertIntoTable(Item item)
+        {
+            Console.WriteLine($"Item : {{ \"ItemId\" : {item.ItemId}, \"ShortDesc\" : \"{item.ShortDesc}\" }}");
+           
+        }
+    }
+}
